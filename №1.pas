76a474981909fb50a4e98991b1e7af461877a43a
matthewqx13 <144227151+matthewqx13@users.noreply.github.com>
2023-11-23 17:36:14 +0300

    Add files via upload

---
+++ №1.pas
@@ -0,0 +1,22 @@
+﻿var
+  n, i, max: int64;
+  a: array of Integer;
+
+begin
+  write('Введите количество элементов массива: ');
+  readln(n);
+  
+  setlength(a, n);
+  
+  write('Введите элементы массива: ');
+  for i := 0 to n - 1 do
+    read(a[i]);
+  
+  max := abs(a[0]);
+  for i := 1 to n - 1 do
+    if abs(a[i]) > max then
+      max := abs(a[i]);
+  
+  writeln('Максимальное по модулю значение в массиве: ', max);
+  
+end.
---
+++ №2.pas
@@ -0,0 +1,27 @@
+﻿var
+  str1, str2: string;
+  i, j: integer;
+
+begin
+  write('Введите исходную строку: ');
+  readln(str1);
+  
+  write('Введите строку ввода: ');
+  readln(str2);
+  
+  i := 1;
+  j := 1;
+  
+  for i := 1 to length(str1) do
+  begin
+    if str1[i] = str2[j] then
+    begin
+      j := j + 1;
+    end;
+  end;
+  
+  if j > length(str2) then
+    writeln('Входная строка является подпоследовательностью исходной строки')
+  else
+    writeln('Входная строка не является подпоследовательностью исходной строки');
+end.
